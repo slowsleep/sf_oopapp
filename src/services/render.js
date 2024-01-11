515fe3488db01478a40f99c5ac9f736923872ed1
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-01-11 22:11:27 +0300

    Add rendering of tasks of user

--- src/services/render.js
+++ src/services/render.js
@@ -2,16 +2,35 @@ import taskFieldTemplate from "../templates/taskField.html";
 import formLoginned from "../templates/nav/right/formLoginned.html";
 import formUnloginned from "../templates/nav/right/formUnloginned.html";
 import tasksCounters from "../templates/footer/tasksCounters.html";
-
+import { appState } from "../app";
+import * as TaskController from "../controllers/TaskController";
 
 export function content(isAuth) {
     if (isAuth) {
-      document.querySelector("#content").innerHTML = taskFieldTemplate;
+        document.querySelector("#content").innerHTML = taskFieldTemplate;
+        let TasksListBacklog = document.querySelector(
+            "#app-tasks-list-backlog"
+        );
+        let user = appState.currentUser;
+
+        if (user) {
+            let usersTasks = TaskController.getUsersTasks(user.id);
+            if (usersTasks) {
+                for (let task of usersTasks) {
+                    if (task.status == "backlog") {
+                        let taskToAppend = document.createElement("li");
+                        taskToAppend.textContent = task.title;
+                        TasksListBacklog.appendChild(taskToAppend);
+                    }
+                    // TODO: and then append another tasks by status
+                }
+            }
+        }
     } else {
-      alert("Доступ запрещен!");
-      window.location.reload();
+        alert("Доступ запрещен!");
+        window.location.reload();
     }
-  }
+}
 
 export function navRight(isAuth) {
     let fieldHTMLContent = isAuth ? formLoginned : formUnloginned;
