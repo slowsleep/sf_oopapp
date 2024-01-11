0dd29d46157e188b03483a483af430c9e0263551
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-01-11 21:55:47 +0300

    Add currentUser to appState if user exists in localStorage

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ import * as listener from "./services/listener";
 import { User } from "./models/User";
 import { generateTestUser } from "./utils";
 import { State } from "./state";
+import { getFromStorage } from "./utils";
 
 export const appState = new State();
 
 generateTestUser(User);
 
+let user = getFromStorage("user");
+
 if (localStorage.getItem("user")) {
+  appState.currentUser = user;
   render.content(true);
   render.navRight(true);
   render.footer();
