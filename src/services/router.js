5273ed7709a6e49d60a6a45e2e473633e099ca31
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-01-22 01:38:55 +0300

    Edit routing for github and logout

--- src/services/router.js
+++ src/services/router.js
@@ -2,13 +2,19 @@ import * as render from "./render";
 
 export function route(appState) {
     let curPath = window.location.pathname;
+    let href = window.location.href;
+    let hrefSplit = href.split("/");
+    // 3 - for localhost
+    // curPath = hrefSplit[3];
+    // 4 - for github
+    curPath = hrefSplit[4];
 
     switch (curPath) {
-        case "/":
+        case "":
             return render.content(appState);
-        case "/profile":
+        case "profile":
             return render.profile(appState);
-        case "/users":
+        case "users":
             if (appState.currentUser.role == "admin") {
                 return render.adminUsers(appState);
             }
