ef4c6a286bda81a4bf5ca4f6a2214f8f708f668b
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-02-12 17:21:01 +0300

    Fix router for localhost and github pages

--- src/services/router.js
+++ src/services/router.js
@@ -1,18 +1,27 @@
 import * as render from "./render";
 
 export function route(appState) {
-    let curPath = window.location.pathname;
+    let curPath;
+    let path = window.location.pathname;
+    let arrPath = path.split("/");
+
+    if (arrPath.length == 2) {
+        curPath = arrPath[1]
+    } else if (arrPath.length == 3) {
+        curPath = arrPath[2]
+    }
 
     switch (curPath) {
-        case "/":
+        case "":
             return render.content(appState);
-        case "/profile":
+        case "profile":
             return render.profile(appState);
-        case "/users":
+        case "users":
             if (appState.currentUser.role == "admin") {
                 return render.adminUsers(appState);
             }
         default:
             return render.notFound();
     }
+
 }
